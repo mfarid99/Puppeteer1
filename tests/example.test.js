9c2bbbb3272fe1e9b30dcc1f29e8b5812f24fc12
Moe <moe@triblio.com>
2021-05-03 15:49:01 -0400

    7

--- tests/example.test.js
+++ tests/example.test.js
@@ -6,18 +6,42 @@ var google = "https://www.google.com/"
 var devExpress = "https://devexpress.github.io/testcafe/example/"
 var messageA = "Howdy, my name is Moe"
 var messageB = "Message 2"
+var webAppSecurity = "http://zero.webappsecurity.com/"
 
 describe('My First Puppeteer Test', ()=> {
+  let browser
+  let page
+
+  before (async function () {
+    //to setup browser and configs
+    browser = await puppeteer.launch ({
+      headless: false, 
+      // slowMo: 50,
+  })
+
+  page = await browser.newPage()
+  await page.setDefaultTimeout(10000)
+  await page.setDefaultNavigationTimeout(20000)
+
+  })
+
+  after (async function () {
+    //to setup closing browser
+    await browser.close()
+  })
+
+  beforeEach(async function () {
+    //runs before each test step
+
+  })
+
+  afterEach(async function () {
+    //runs after each test step
+  })
+
   it('should launch the browser', async function () {
-      
+    
 
-
-    const browser = await puppeteer.launch ({
-      headless: true, 
-  
-     })
-
-    const page = await browser.newPage()
     await page.goto(pageExample)
     const title = await page.title()
     const url = await page.url()
@@ -32,6 +56,12 @@ describe('My First Puppeteer Test', ()=> {
     expect(text).to.be.a('string', 'Example Domain')
     expect(par).to.include('in literature')
     expect(count).to.equal(2)
+
+    await page.goto(webAppSecurity)
+    await page.waitForSelector('#searchTerm')
+    await page.type('#searchTerm', 'Hello World')
+    await page.keyboard.press('Enter')
+
     await browser.close()
     
   })
